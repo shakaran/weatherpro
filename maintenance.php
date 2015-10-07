963d4547afa6c73f56adac067927a7fa422de112
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-10-07 05:10:31 +0200

    <bugfix> Cosmetic changes

--- maintenance.php
+++ maintenance.php
@@ -8,15 +8,19 @@ $pageVersion	= '3.00 2015-05-23';    // added FR split
 #
 $SITE['wsModules'][$pageName] = 'version: ' . $pageVersion;
 $pageFile = basename(__FILE__);			// check to see this is the real script
-if ($pageFile <> $pageName) {
+if ($pageFile <> $pageName) 
+{
 	$SITE['wsModules'][$pageFile]	= 'this file loaded instead of '.$pageName;
 }
+
 #-------------------------------------------------------------------------------
 # Check if you want a message such as "Site is in maintenance"  if there is such a file found 
 #-------------------------------------------------------------------------------
-if (isset ($SITE['maintenanceTxt']) && file_exists($SITE['maintenanceTxt']) ) {
+if (isset ($SITE['maintenanceTxt']) && file_exists($SITE['maintenanceTxt']) ) 
+{
         $ownWarning     = file_get_contents($SITE['maintenanceTxt']);
-        if (strlen($ownWarning) >= 4 && substr($ownWarning,0,4) <> 'none') {
+        if (strlen($ownWarning) >= 4 && substr($ownWarning,0,4) <> 'none') 
+        {
                 $ownWarning     = iconv('UTF-8',$SITE['charset'].'//TRANSLIT', $ownWarning );
                 echo '<div class="warnBox">'.PHP_EOL.$ownWarning.PHP_EOL.'</div>'.PHP_EOL;
         } // eo check warning file is to be displayed
