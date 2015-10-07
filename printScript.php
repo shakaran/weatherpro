8ee98507e747b36df9bad00da23d78738ae40cdd
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-10-07 03:19:11 +0200

    <bugfix> Cosmetic changes in printScript.php

--- printScript.php
+++ printScript.php
@@ -1,8 +1,10 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
-ini_set('display_errors', 'On'); error_reporting(E_ALL); 
+ini_set('display_errors', 'On'); 
+error_reporting(E_ALL); 
 
-if (isset($_REQUEST['sce']) && strtolower($_REQUEST['sce']) == 'echo' ) {
+if (isset($_REQUEST['sce']) && strtolower($_REQUEST['sce']) == 'echo' ) 
+{
    $filenameReal = __FILE__;
    $download_size = filesize($filenameReal);
    header('Pragma: public');
@@ -15,24 +17,38 @@ if (isset($_REQUEST['sce']) && strtolower($_REQUEST['sce']) == 'echo' ) {
    readfile($filenameReal);
    exit;
 }
+
 $pageName	= 'printScript.php';
-if (!isset($_REQUEST['echo']) || strtolower($_REQUEST['echo']) == '' ) {
+
+if (!isset($_REQUEST['echo']) || strtolower($_REQUEST['echo']) == '' ) 
+{
         echo 'na valida specas'; exit;
 }
-ob_start(); if (!is_array($SITE) ) { $SITE= array();} include_once 'wsLoadSettings.php'; ob_end_clean();
+
+ob_start(); 
+if (!is_array($SITE) ) 
+{
+	 $SITE= array();
+} 
+include_once 'wsLoadSettings.php'; 
+ob_end_clean();
 #
-if (isset($SITE['password']) && trim($SITE['password']) <> '' ) {
+if (isset($SITE['password']) && trim($SITE['password']) <> '' ) 
+{
         $pass   = trim( $SITE['password'] );
-        if (!isset($_REQUEST['pw'])  )   {
+        if (!isset($_REQUEST['pw'])  )   
+        {
                 echo 'na valida spepwas'; 
                 exit;
         }
+		
         $pw     = trim($_REQUEST['pw']);
         if ($pass <> $pw)               {
                 echo 'na valida spepwas-2'; 
                 exit;
         }  
 }
+
 $filenameReal = trim($_REQUEST['echo']);
 
 $download_size = filesize($filenameReal);
@@ -44,4 +60,3 @@ header("Accept-Ranges: bytes");
 header("Content-Length: $download_size");
 header('Connection: close');
 readfile($filenameReal);
-?>
